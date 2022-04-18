fc92b56c792064e296ed35fa60cc5d21eae857b3
booldook <booldook@gmail.com>
2022-04-18 18:31:50 +0900

    06.state

--- js/06.state.js
+++ js/06.state.js
@@ -1,10 +1,7 @@
 class App extends React.Component {
   state = {
-    size: 500,
+    size: 600,
     src: 'https://placeimg.com/600/600/any'
-  }
-  onChange = (e) => {
-    this.size = e.target.value;
   }
   onClick = (e) => {
     const size = this.state.size - 50
@@ -19,7 +16,6 @@ class App extends React.Component {
         <div></div>
         <img src={this.state.src}/>
         <div>
-          {/* <input type="text" onChange={onChange} />           */}
           <button className="btn btn-primary" onClick={this.onClick}>바꿔</button>
         </div>
       </div>
